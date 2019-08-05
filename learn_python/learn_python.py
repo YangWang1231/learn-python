2ab48c37be73290f78faae82938f43ea367bae11
pene_wang <pene_wang@126.com>
2019-08-05 13:54:30 +0800

    first commit

--- learn_python/learn_python.py
+++ learn_python/learn_python.py
@@ -1,1 +1,24 @@
+import logging
+
+
+
+#闭包的理解
+def use_logging(func):
+    '''
+    闭包的调用时机，是在使用闭包decorat一个函数的时候
+    '''
+    print("%s was decorated." % func.__name__)     #表述闭包时机
+    def wrapper(*arg, **kwargs):
+        logging.warn("%s is running" % func.__name__)
+        return func(*arg, **kwargs)
+    return wrapper
+
+@use_logging
+def bar():
+    print("i am bar")
+
+print("before call decorated function.")
+bar()
+
+
 
